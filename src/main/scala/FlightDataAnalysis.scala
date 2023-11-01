18bed01f93162b35df0308cd7a829d0c2142d9b7
MarioFialho <mgf2007@gmail.com>
2023-11-01 18:15:57 -0400

    Scaladocs Added, all choices complete successfully

--- src/main/scala/FlightDataAnalysis.scala
+++ src/main/scala/FlightDataAnalysis.scala
@@ -4,16 +4,28 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 import java.sql.Date
-
+/**
+ * Object to analyze flight data.
+ */
 object FlightDataAnalysis {
 
+  /** Case class representing a flight. */
   case class Flight(passengerId: String, flightId: String, from: String, to: String, date: Date)
+  /** Case class representing a Passenger. */
   case class Passenger(passengerId: String, firstName: String, lastName: String)
+  /** Case class representing a pair of flights. */
   case class FlightPair(passenger1Id: String, passenger2Id: String, flightId: String, date: Date)
+  /** Case class representing a pair of passengers. */
   case class PassengerPair(passenger1Id: String, passenger2Id: String)
 
 
-
+  /**
+   * Answers question 1: Counts the number of flights per month.
+   *
+   * @param flights The dataset of flights.
+   * @param spark The Spark session.
+   * @return A DataFrame with two columns: Month and Number of Flights.
+   */
   def answerQuestion1(flights: Dataset[Flight])(spark: SparkSession): DataFrame = {
     import spark.implicits._
     flights
@@ -22,7 +34,14 @@ object FlightDataAnalysis {
       .toDF("Month", "Number of Flights")
       .sort("Month")
   }
-
+  /**
+   * Answers question 2: Gets the top 100 passengers who have flown the most.
+   *
+   * @param flights The dataset of flights.
+   * @param passengers The dataset of passengers.
+   * @param spark The Spark session.
+   * @return A DataFrame with the passenger details and the number of flights they've taken.
+   */
   def answerQuestion2(flights: Dataset[Flight], passengers: Dataset[Passenger])(spark: SparkSession): DataFrame = {
     import spark.implicits._
     flights
@@ -34,17 +53,31 @@ object FlightDataAnalysis {
       .sort(desc("Number of Flights"), asc("Passenger ID"))
       .limit(100)
   }
-  // Function to compute the longest run of countries without visiting the UK
-  def longestRun(countries: Seq[String]): Int = {
+  /**
+   * Computes the longest run of countries visited without visiting the UK.
+   *
+   * @param countries A sequence of country codes.
+   * @return The length of the longest run.
+   */  def longestRun(countries: Seq[String]): Int = {
     countries.foldLeft((0, 0)) { case ((currentRun, maxRun), country) =>
       if (country == "UK") (0, maxRun)
       else (currentRun + 1, Math.max(currentRun + 1, maxRun))
     }._2
   }
 
-  // Registering the function as a Spark UDF
+  /**
+   * Registers longestRun function as a Spark UDF.
+   *
+   * @return The UserDefinedFunction.
+   */
   def longestRunUDF: UserDefinedFunction = udf(longestRun _)
-
+  /**
+   * Answers question 3: Finds the longest run of countries visited without visiting the UK for each passenger.
+   *
+   * @param flights The dataset of flights.
+   * @param spark The Spark session.
+   * @return A DataFrame with Passenger ID and their Longest Run.
+   */
   def answerQuestion3(flights: Dataset[Flight])(spark: SparkSession): DataFrame = {
     import spark.implicits._
 
@@ -58,7 +91,13 @@ object FlightDataAnalysis {
       .sort(desc("Longest Run"))
       .toDF("Passenger ID", "Longest Run")
   }
-
+  /**
+   * Answers question 4: Finds passenger pairs that have flown together more than 3 times.
+   *
+   * @param flights The dataset of flights.
+   * @param spark The implicit Spark session.
+   * @return A Dataset of Passenger Pairs and the number of flights they've flown together.
+   */
   def answerQuestion4(flights: Dataset[Flight])(implicit spark: SparkSession): Dataset[(PassengerPair, Long)] = {
     import spark.implicits._
 
@@ -79,6 +118,16 @@ object FlightDataAnalysis {
       .as[(PassengerPair, Long)]
   }
 
+  /**
+   * Answers question 5: Finds passenger pairs that have flown together at least a certain number of times within a date range.
+   *
+   * @param flights       The dataset of flights.
+   * @param atLeastNTimes The minimum number of times the passenger pairs should have flown together.
+   * @param from          The start date of the date range.
+   * @param to            The end date of the date range.
+   * @param spark         The Spark session.
+   * @return A DataFrame with passenger pair details, number of flights together, and the date range.
+   */
   def answerQuestion5(flights: Dataset[Flight], atLeastNTimes: Int, from: Date, to: Date)(spark: SparkSession): DataFrame = {
     import spark.implicits._
 
